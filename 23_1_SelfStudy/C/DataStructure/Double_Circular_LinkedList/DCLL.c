22675ada620ca71de3e753845bb30d75cce0b00f
SeHIgh <sehi0119@naver.com>
2023-04-13 19:24:51 +0900

    2023 Code work

--- 23_1_SelfStudy/C/DataStructure/Double_Circular_LinkedList/DCLL.c
+++ 23_1_SelfStudy/C/DataStructure/Double_Circular_LinkedList/DCLL.c
@@ -34,7 +34,7 @@ Node* LSearch(List* plist, Data data);	//특정 data를 가지는 노드를 탐색 및 주소 
 int main(void) {
 	Data data = 0, fdata = 0;
 	List list;
-	Node* find = LSearch(&list, fdata);
+	Node* find;
 
 	LInit(&list);
 	for (int i = 1; i <= 5; i++)
@@ -42,10 +42,14 @@ int main(void) {
 
 	if (LFirst(&list, &data)) {
 		printf("%d ",data);
-		while (LNext(&list, &data))
-			printf("%d ", data);
+		while (LNext(&list, &data)) {
+			if (list.cur != list.head)					//circular이기 때문에 일반 양방향연결리스트 출력을 쓰면, 무한정 루프를 돌게됨. 따라서, cur이 head를 만나면 중지하도록 설정.
+				printf("%d ", data);
+			else
+				break;
+		}
 	}
-
+	printf("삭제 하고자 하는 수를 입력하시오 : ");
 	scanf("%d", &fdata);
 	find = LSearch(&list, fdata);
 
@@ -53,10 +57,27 @@ int main(void) {
 
 	if (LFirst(&list, &data)) {
 		printf("%d ", data);
-		while (LNext(&list, &data))
-			printf("%d ", data);
+		while (LNext(&list, &data)) {
+			if (list.cur != list.head)
+				printf("%d ", data);
+			else
+				break;
+		}
 	}
 
+	printf("삽입 하고자 하는 위치(a -> a뒤)와 수를 입력하시오 : ");
+	scanf("%d %d", &fdata, &data);
+	find = LSearch(&list, fdata);
+	LInsert_Middle(&list, find, data);
+	if (LFirst(&list, &data)) {
+		printf("%d ", data);
+		while (LNext(&list, &data)) {
+			if (list.cur != list.head)
+				printf("%d ", data);
+			else
+				break;
+		}
+	}
 	return 0;
 }
 
@@ -74,13 +95,13 @@ void LInsert(List* plist, Data data) {
 		newNode->llink = newNode;
 		newNode->rlink = newNode;
 		plist->head = newNode;
-		exit(-1);
 	}
-
-	newNode->llink = plist->head;
-	newNode->rlink = plist->head;
-	plist->head = newNode;
-
+	else {												//헤드의 왼쪽(리스트의 맨 끝에 삽입한다 생각)
+		newNode->llink = plist->head->llink;
+		newNode->rlink = plist->head;
+		plist->head->llink->rlink = newNode;
+		plist->head->llink = newNode;
+	}
 	(plist->numOfData)++;
 }
 
@@ -89,25 +110,25 @@ void LInsert_Middle(List* plist, Node* pprev, Data data) {
 	newNode->data = data;
 
 	if (plist->head == NULL) {					//선행 노드가 존재하지 않는 경우(탐색 함수에서 만져줘야함) -> pprev == list->head
-		printf("시작1");
 		newNode->llink = newNode;
 		newNode->rlink = newNode;
 		plist->head = newNode;
-		printf("종료1\n");
-		exit(-1);
 	}
-	printf("시작2\n");
-	newNode->rlink = pprev->rlink;
-	newNode->llink = pprev;
-	pprev->rlink->llink = newNode;
-	pprev->rlink = newNode;
-	printf("종료2\n");
+	else {
+		newNode->rlink = pprev->rlink;
+		newNode->llink = pprev;
+		pprev->rlink->llink = newNode;
+		pprev->rlink = newNode;
+	}
 	(plist->numOfData)++;
 }
-void LDelete(List* plist, Node* pdel) {				//삭제
+void LDelete(List* plist, Node* pdel) {				//삭제	//문제점 : head 노드를 삭제 할때 오류가 생김
 	if (pdel != NULL) {
 		pdel->llink->rlink = pdel->rlink;
 		pdel->rlink->llink = pdel->llink;
+	}
+	if (pdel == plist->head) {								//해결방법!
+		plist->head = pdel->rlink;							//헤더 노드가 삭제되서 생기는 문제, 헤더노드를 그다음노드로 지정
 	}
 	free(pdel);
 
@@ -151,9 +172,8 @@ int LCount(List* plist) {				//List를 구성하는 노드의 개수 반환
 Node* LSearch(List* plist, Data fdata) {	//특정 data를 가지는 노드를 탐색 및 주소 반환
 	Node* Find;
 	Find = plist->cur;
-	if (fdata == 0) {
+	if (plist->head == NULL)
 		return Find;
-	}
 	while (Find->data != fdata) {
 		Find = Find->rlink;
 	}
