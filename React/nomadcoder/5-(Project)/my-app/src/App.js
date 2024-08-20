2a5453d673b6d9a21a8fc3c5c8f8cafb60b7659b
SeHyeon <sehi@naver.com>
2024-08-20 22:08:44 +0900

    React / 5.4 Cleanup

--- React/nomadcoder/5-(Project)/my-app/src/App.js
+++ React/nomadcoder/5-(Project)/my-app/src/App.js
@@ -1,43 +1,26 @@
 import { useState, useEffect } from "react";
 
-function App() {
-  const [counter, setValue] = useState(0);
-  const onClick = () => setValue((prev) => prev + 1);
-  const [keyword, setKeyword] = useState("");
-  const onChange = (event) => setKeyword(event.target.value);
-
-  useEffect(() => {
-    console.log("I run only once.");
+function Hello(){
+  useEffect(function(){
+    console.log("hi :)");
+    return function(){
+      console.log("bye :(");
+    };
   }, []);
+  useEffect(()=>{
+    console.log("hi :)");
+    return () => console.log("bye :(");
+  },[]);
+  return <h1>Hello</h1>;
+}
 
-  useEffect(() => {
-    if (counter > 0){
-      console.log("I run when 'counter' changes.");
-    }
-  }, [counter]);
-
-  useEffect(() => {
-    if (keyword != "" && keyword.length > 5) {
-      console.log("Search for", keyword);
-    }
-  }, [keyword]);
-  
-  useEffect(() => {
-    if (keyword != "" && counter > 0) {
-      console.log("keyword & counter changes.");
-    }
-  }, [keyword, counter]);
-
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => {setShowing((prev)=> !prev)};
   return (
     <div>
-      <input
-        value={keyword}
-        type="text"
-        onChange={onChange}
-        placeholder="Search here..."
-      ></input>
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      <button onClick={onClick}>{showing ? "Hide" : "show"}</button>
+      {showing? <Hello /> : null}
     </div>
   );
 }
