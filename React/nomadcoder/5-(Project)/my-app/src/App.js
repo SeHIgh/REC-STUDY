2d992da4b225bf0e2a286355724f70e73f00d8b0
SeHyeon <sehi@naver.com>
2024-08-21 17:20:34 +0900

    React / 7.0 | To Do List : 단일 To Do 항목 submit & 제출 후 초기화

--- React/nomadcoder/5-(Project)/my-app/src/App.js
+++ React/nomadcoder/5-(Project)/my-app/src/App.js
@@ -1,26 +1,29 @@
 import { useState, useEffect } from "react";
 
-function Hello(){
-  useEffect(function(){
-    console.log("hi :)");
-    return function(){
-      console.log("bye :(");
-    };
-  }, []);
-  useEffect(()=>{
-    console.log("hi :)");
-    return () => console.log("bye :(");
-  },[]);
-  return <h1>Hello</h1>;
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => {setShowing((prev)=> !prev)};
+  const [toDo, setToDo] = useState("");
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault(); 
+    if (toDo === ""){
+      return;
+    }
+    setToDo("");
+    console.log(toDo);
+  }
   return (
     <div>
-      <button onClick={onClick}>{showing ? "Hide" : "show"}</button>
-      {showing? <Hello /> : null}
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>
+          Add To Do
+        </button>
+      </form>
     </div>
   );
 }
