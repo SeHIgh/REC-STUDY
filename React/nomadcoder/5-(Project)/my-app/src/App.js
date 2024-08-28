4fa3e16248d60df22c66e7769c96911788aa234f
SeHIgh <sehi0119@naver.com>
2024-08-28 12:47:29 +0900

    React / 7.3 Movie App part One : API 를 통한 movie 목록 받아오기 ( .then 이용)

--- React/nomadcoder/5-(Project)/my-app/src/App.js
+++ React/nomadcoder/5-(Project)/my-app/src/App.js
@@ -2,50 +2,19 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [money, setMoney] = useState("");
-  const [changed, setChanged] = useState(null);
+  const [movies, setMovies] = useState([]);
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
+    fetch(
+      "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
+    )
       .then((response) => response.json())
       .then((json) => {
-        setCoins(json);
+        setMovies(json.data.movies);
         setLoading(false);
       });
   }, []);
-  const onChange = (event) => setMoney(event.target.value);
-  const moneyChange = (event) => {
-    const selectedCoin = coins.find(
-      (coin) => coin.id === event.target.value
-    );
-    setChanged(Math.floor(money / selectedCoin.quotes.USD.price));
-  };
-  return (
-    <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
-      {loading ? (
-        <strong>Loading...</strong>
-      ) : (
-        <div>
-          <input
-            type="text"
-            onChange={onChange}
-            value={money}
-            placeholder="코인 환전 원하는 달러 입력..."
-          ></input>
-          <select onChange={moneyChange}>
-            <option value="">코인 선택...</option>
-            {coins.map((coin) => (
-              <option value={coin.id}>
-                {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
-              </option>
-            ))}
-          </select>
-          <h3>{changed != null ? `${changed} 개` : null}</h3>
-        </div>
-      )}
-    </div>
-  );
+  console.log(movies);
+  return <div>{loading ? <h1>Loading...</h1> : null}</div>;
 }
 
 export default App;
