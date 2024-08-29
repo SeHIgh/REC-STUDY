52a75bc892159a15e4a228a500ddff6b481e55f1
SeHIgh <sehi0119@naver.com>
2024-08-29 17:16:02 +0900

    React / 7.5 React Router : 라우터를 이용한 페이지 연결 및 주소 설정 & v6 이후로 바뀐 변경점

--- React/nomadcoder/5-(Project)/my-app/src/App.js
+++ React/nomadcoder/5-(Project)/my-app/src/App.js
@@ -1,40 +1,16 @@
-import { useEffect, useState } from "react";
-import Movie from "./Movie";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const json = await (
-      await fetch(
-        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
-      )
-    ).json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-  useEffect(() => {
-    getMovies();
-  }, []);
   return (
-    <div>
-      {loading ? (
-        <h1>Loading...</h1>
-      ) : (
-        <div>
-          <h1>영화 목록 ({movies.length})</h1>
-          {movies.map((movie) => (
-            <Movie
-              key={movie.id}
-              coverImg={movie.medium_cover_image}
-              title={movie.title}
-              summary={movie.summary}
-              genres={movie.genres}
-            />
-          ))}
-        </div>
-      )}
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/hello" element={<h1>Hello :)</h1>}/>
+        <Route path="/movie" element={<Detail />}/>
+        <Route path="/" element={<Home />}/>
+      </Routes>
+    </Router>
   );
 }
 
