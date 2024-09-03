5b137347426c9a2668ef3d946cbb8b9061634f63
SeHyeon <sehi@naver.com>
2024-09-03 22:14:39 +0900

    React / React-masterClass : TS & State 관계

--- React/ReactMasterCL/react-ts/src/Circle.tsx
+++ React/ReactMasterCL/react-ts/src/Circle.tsx
@@ -1,21 +1,21 @@
+import { useState } from "react";
 import styled from "styled-components";
 
-interface ContainerProps {
+interface CircleProps {
   bgColor: string;
 }
 
-const Container = styled.div<ContainerProps>`
+const Container = styled.div<CircleProps>`
   width: 200px;
   height: 200px;
   background-color: ${(props) => props.bgColor};
   border-radius: 100px;
 `;
 
-interface CircleProps {
-  bgColor: string;
-}
-
 function Circle({ bgColor }: CircleProps) {
+//   const [value, setValue] = useState<number|string>(0);
+//   setValue("hello");
+
   return <Container bgColor={bgColor} />;
 }
 
@@ -23,10 +23,11 @@ export default Circle;
 
 interface PlayerShape {
   name: string;
-  age: number;
+  age: number|string;
 }
 
 const sayHello = (playerObj: PlayerShape) =>
   alert(`Hello ${playerObj.name} you are ${playerObj.age} years old.`);
 
 sayHello({ name: "sehi", age: 24 });
+sayHello({ name: "sehi", age: "24" });
