19b13051a2fff3ca7cdbf17f56798fcae96ec4f5
SeHyeon <sehi@naver.com>
2024-09-03 22:03:15 +0900

    React / React-masterClass : interface를 이용 예시

--- React/ReactMasterCL/react-ts/src/Circle.tsx
+++ React/ReactMasterCL/react-ts/src/Circle.tsx
@@ -1,22 +1,32 @@
 import styled from "styled-components";
 
-interface ContainerProps{
-    bgColor: string;
+interface ContainerProps {
+  bgColor: string;
 }
 
 const Container = styled.div<ContainerProps>`
-    width: 200px;
-    height: 200px;
-    background-color: ${(props) => props.bgColor};
-    border-radius: 100px;
+  width: 200px;
+  height: 200px;
+  background-color: ${(props) => props.bgColor};
+  border-radius: 100px;
 `;
 
 interface CircleProps {
-    bgColor: string;
+  bgColor: string;
 }
 
-function Circle({bgColor}:CircleProps){
-    return <Container bgColor={bgColor} />;
+function Circle({ bgColor }: CircleProps) {
+  return <Container bgColor={bgColor} />;
 }
 
 export default Circle;
+
+interface PlayerShape {
+  name: string;
+  age: number;
+}
+
+const sayHello = (playerObj: PlayerShape) =>
+  alert(`Hello ${playerObj.name} you are ${playerObj.age} years old.`);
+
+sayHello({ name: "sehi", age: 24 });
