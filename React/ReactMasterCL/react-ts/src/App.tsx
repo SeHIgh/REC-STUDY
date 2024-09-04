fe0ccb29889000cf4c77726d5bb5f627d7710c30
SeHyeon <sehi@naver.com>
2024-09-04 22:19:14 +0900

    auto upload

--- React/ReactMasterCL/react-ts/src/App.tsx
+++ React/ReactMasterCL/react-ts/src/App.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
-  background-color: ${(props) => props.theme.bggggggColor};
+  background-color: ${(props) => props.theme.bgColor};
 `;
 const H1 = styled.h1`
   color: ${(props) => props.theme.textColor};
