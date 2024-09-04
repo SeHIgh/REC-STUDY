753ff7bafe93faee7d17fb736a3eff6cc0f63d62
SeHIgh <sehi0119@naver.com>
2024-09-04 10:02:21 +0900

    React / React-MasterClass : React with TS Forms

--- React/ReactMasterCL/react-ts/src/App.tsx
+++ React/ReactMasterCL/react-ts/src/App.tsx
@@ -1,7 +1,30 @@
-import styled from "styled-components";
+import React, { useState } from "react";
 
 function App() {
-  return <div></div>;
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+  };
+  const onSubmit = (event:React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("hello", value);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={value}
+          onChange={onChange}
+          type="text"
+          placeholder="username"
+        />
+        <button>Login</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
