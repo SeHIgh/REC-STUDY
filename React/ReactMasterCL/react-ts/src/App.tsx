0ee08273965220f2b36c614de016302f10873c91
SeHIgh <sehi0119@naver.com>
2024-09-05 09:03:17 +0900

    Auto Upload

--- React/ReactMasterCL/react-ts/src/App.tsx
+++ React/ReactMasterCL/react-ts/src/App.tsx
@@ -1,19 +1,7 @@
-import React, { useState } from "react";
-import styled from "styled-components";
+import { Router } from "react-router-dom";
 
-const Container = styled.div`
-  background-color: ${(props) => props.theme.bgColor};
-`;
-const H1 = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
-
-function App() {
-  return (
-    <Container>
-      <H1>protected</H1>
-    </Container>
-  );
+function App(){
+  return <Router />;
 }
 
 export default App;
