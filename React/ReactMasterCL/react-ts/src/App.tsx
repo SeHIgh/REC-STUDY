0897fae20267d57e3ea397d133d62a444e5ae4bf
SeHIgh <sehi0119@naver.com>
2024-09-08 12:55:21 +0900

    React / Coin Tracker : styled-reset 적용 및 폰트 적용 & 폰트 적용시 발생하는 문제 해결 -> react-helmet-async 라이브러리 이용

--- React/ReactMasterCL/react-ts/src/App.tsx
+++ React/ReactMasterCL/react-ts/src/App.tsx
@@ -1,7 +1,78 @@
+import { createGlobalStyle } from "styled-components";
 import Router from "./Router";
+import { Helmet } from "react-helmet-async";
 
-function App(){
-  return <Router />;
+const GlobalStyle = createGlobalStyle`
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, menu, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed,
+figure, figcaption, footer, header, hgroup,
+main, menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-size: 100%;
+  font: inherit;
+  vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure,
+footer, header, hgroup, main, menu, nav, section {
+  display: block;
+}
+/* HTML5 hidden-attribute fix for newer browsers */
+*[hidden] {
+    display: none;
+}
+body {
+  line-height: 1;
+}
+menu, ol, ul {
+  list-style: none;
+}
+blockquote, q {
+  quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+  content: '';
+  content: none;
+}
+table {
+  border-collapse: collapse;
+  border-spacing: 0;
+}
+*{
+  box-sizing: border-box;
+}
+body {
+  font-family: "Source Sans 3", sans-serif;
+  background-color:${(props) => props.theme.bgColor};
+  color:${(props) => props.theme.textColor};
+}
+a {
+  text-decoration: none;
+}
+`;
+
+function App() {
+  return (
+    <>
+      <Helmet>
+        <link href="https://fonts.googleapis.com/css2?family=Source+Sans+3:ital,wght@0,200..900;1,200..900&display=swap" />
+      </Helmet>
+      <GlobalStyle />
+      <Router />
+    </>
+  );
 }
 
 export default App;
--- React/ReactMasterCL/react-ts/src/theme.ts
+++ React/ReactMasterCL/react-ts/src/theme.ts
@@ -1,7 +1,7 @@
 import { DefaultTheme } from "styled-components/dist/types";
 
 export const theme: DefaultTheme = {
-  bgColor: "white",
-  textColor: "black",
-  btnColor: "tomato",
+  bgColor: "#2c3e50",
+  textColor: "#ecf0f1",
+  accentColor: "#2ecc71",
 };
