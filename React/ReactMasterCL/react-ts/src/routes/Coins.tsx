16c808a032dcebf2b701cba169189560678c70ec
SeHIgh <sehi0119@naver.com>
2024-09-08 13:10:41 +0900

    React / Coin Tracker : mapping Coins List & styles

--- React/ReactMasterCL/react-ts/src/routes/Coins.tsx
+++ React/ReactMasterCL/react-ts/src/routes/Coins.tsx
@@ -1,11 +1,71 @@
 import styled from "styled-components";
 
-const Title = styled.h1`
-    color:${props => props.theme.accentColor};
+const Container = styled.div`
+  padding: 0px 20px;
+`;
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const CoinsList = styled.ul``;
+const Coin = styled.li`
+    background-color: white;
+    color: ${(props) => props.theme.bgColor};
+    padding: 20px;
+    border-radius: 15px;
+    margin-bottom: 10px;
 `;
 
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const coins = [
+  {
+    id: "btc-bitcoin",
+    name: "Bitcoin",
+    symbol: "BTC",
+    rank: 1,
+    is_new: false,
+    is_active: true,
+    type: "coin",
+  },
+  {
+    id: "eth-ethereum",
+    name: "Ethereum",
+    symbol: "ETH",
+    rank: 2,
+    is_new: false,
+    is_active: true,
+    type: "coin",
+  },
+  {
+    id: "usdt-tether",
+    name: "Tether",
+    symbol: "USDT",
+    rank: 3,
+    is_new: false,
+    is_active: true,
+    type: "token",
+  },
+];
+
 function Coins() {
-    return <Title>코인</Title>;
+  return (
+    <Container>
+      <Header>
+        <Title>코인</Title>
+      </Header>
+      <CoinsList>
+        {coins.map((coin) => (
+          <Coin key={coin.id}>{coin.name} &rarr;</Coin>
+        ))}
+      </CoinsList>
+    </Container>
+  );
 }
 
 export default Coins;
