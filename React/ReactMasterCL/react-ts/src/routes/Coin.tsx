6f98e08a22ed010c0d05c756e7e0a405f5ad027d
SeHIgh <sehi0119@naver.com>
2024-09-11 19:22:12 +0900

    React/Coin Tracker: 코인의 세부정보 데이터 & 가격정보 데이터의 모든 요소 값의 자료형을 지정

--- React/ReactMasterCL/react-ts/src/routes/Coin.tsx
+++ React/ReactMasterCL/react-ts/src/routes/Coin.tsx
@@ -29,64 +29,91 @@ interface RouteState {
   name: string;
 }
 
-interface IInfoData{
-    id:string;
-    name:string;
-    symbol:string;
-    rank:number;
-    is_new:boolean;
-    is_active:boolean;
-    type:string;
-    logo:string;
-    tags:object;
-    team:object;
-    description:string;
-    message:string;
-    open_source:boolean;
-    started_at:string;
-    development_status:string;
-    hardware_wallet:boolean;
-    proof_type:string;
-    org_structure:string;
-    hash_algorithm:string;
-    links:object;
-    links_extended:object;
-    whitepaper:object;
-    first_data_at:string;
-    last_data_at:string;
+// 타입이 배열인 경우 필요하다면 interface 를 이용해서 자료형을 지정해줄 수 있다.
+// interface ITag{
+//   id: string;
+//   name: string;
+//   coin_counter: number;
+//   ico_counter: number;
+// }
+
+interface IInfoData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+  logo: string;
+  // tags:ITag[];
+  // team:object;
+  description: string;
+  message: string;
+  open_source: boolean;
+  started_at: string;
+  development_status: string;
+  hardware_wallet: boolean;
+  proof_type: string;
+  org_structure: string;
+  hash_algorithm: string;
+  links: object;
+  links_extended: object;
+  whitepaper: object;
+  first_data_at: string;
+  last_data_at: string;
 }
 
-interface IPriceData{
-    id: string;
-    name: string;
-    symbol: string;
-    rank: number;
-    total_supply: number;
-    max_supply: number;
-    beta_value: number;
-    first_data_at: string;
-    last_updated: string;
-    quotes: object;
+interface IPriceData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  total_supply: number;
+  max_supply: number;
+  beta_value: number;
+  first_data_at: string;
+  last_updated: string;
+  quotes: {
+    USD: {
+      price: number;
+      volume_24h: number;
+      volume_24h_change_24h: number;
+      market_cap: number;
+      market_cap_change_24h: number;
+      percent_change_15m: number;
+      percent_change_30m: number;
+      percent_change_1h: number;
+      percent_change_6h: number;
+      percent_change_12h: number;
+      percent_change_24h: number;
+      percent_change_7d: number;
+      percent_change_30d: number;
+      percent_change_1y: number;
+      ath_price: number;
+      ath_date: number;
+      percent_from_price_ath: number;
+    };
+  };
 }
 
 function Coin() {
   const [loading, setLoading] = useState(true);
   const { coinId } = useParams<RouteParams>();
   const { state } = useLocation<RouteState>();
-  const [info, setInfo] = useState({});
-  const [priceInfo, setPriceInfo] = useState({});
+  const [info, setInfo] = useState<IInfoData>();
+  const [priceInfo, setPriceInfo] = useState<IPriceData>();
   useEffect(() => {
     (async () => {
       const infoData = await (
         await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
       ).json();
-      console.log(infoData)
       const priceData = await (
         await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
       ).json();
-      console.log(priceData)
       setInfo(infoData);
       setPriceInfo(priceData);
+      setLoading(false);
     })();
   }, []);
   return (
@@ -94,7 +121,11 @@ function Coin() {
       <Header>
         <Title>{state?.name || "Loading..."}</Title>
       </Header>
-      {loading ? <Loader>Loading...</Loader> : <span></span>}{" "}
+      {loading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <span>{priceInfo?.quotes.USD.price}</span>
+      )}
     </Container>
   );
 }
